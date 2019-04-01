b4c99458620d92156d0dafeba3d3d9e43b6573dc
IgnacioCentola <46284424+IgnacioCentola@users.noreply.github.com>
2019-04-01 16:16:28 -0300

    Le di estructura basica a class1.cs

--- class1.cs
+++ class1.cs
@@ -1,3 +1,20 @@
-for(int i=0; i<5; i++){
+
+
+
+namespace Proyecto{
+
+    public class Class1{
+        for(int i=0; i<5; i++){
     Console.WriteLine("Peron");
+   
+        };
+    }
 }
+
+
+
+
+
+
+
+
